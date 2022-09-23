94f641308ba54b08874021f89ebadc850cd54613
Robert Helgeson <son_of_helga@icloud.com~>
2022-09-23 15:48:27 -0700

    changing ufw setup

--- plex-setup.sh
+++ plex-setup.sh
@@ -5,23 +5,7 @@ echo deb https://downloads.plex.tv/repo/deb public main | sudo tee /etc/apt/sour
 sudo apt update -y sudo apt full-upgrade -y
 sudo apt install plexmediaserver -y
 
-sudo touch sudo nano /etc/ufw/applications.d/plexmediaserver
-
-sudo echo "[plexmediaserver]
-title=Plex Media Server (Standard)
-description=The Plex Media Server
-ports=32400/tcp|3005/tcp|5353/udp|8324/tcp|32410:32414/udp
-
-[plexmediaserver-dlna]
-title=Plex Media Server (DLNA)
-description=The Plex Media Server (additional DLNA capability only)
-ports=1900/udp|32469/tcp
-
-[plexmediaserver-all]
-title=Plex Media Server (Standard + DLNA)
-description=The Plex Media Server (with additional DLNA capability)
-ports=32400/tcp|3005/tcp|5353/udp|8324/tcp|32410:32414/udp|1900/udp|32469/tcp
-" > /etc/ufw/applications.d/plexmediaserver
+sudo mv plexmediaserver /etc/ufw/applications.d/plexmediaserver
 
 sudo ufw app update plexmediaserver
 
